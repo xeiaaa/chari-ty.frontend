eac1808f0e01b739ba0d5654bbbc0561049ccbd6
Bret Axl Sebastian Pura <cosmicdevcs@gmail.com>
2025-07-28 11:07:12 +0800

    center signin/signup form

--- src/app/signin/[[...rest]]/page.tsx
+++ src/app/signin/[[...rest]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-background px-4">
+    <div className="h-[calc(100vh-75px)] flex flex-col items-center justify-center bg-background px-4">
       <SignIn signUpUrl="/signup" />
     </div>
   );
--- src/app/signup/[[...rest]]/page.tsx
+++ src/app/signup/[[...rest]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-background px-4">
+    <div className="min-h-[calc(100vh-75px)] flex flex-col items-center justify-center bg-background px-4">
       <div className="p-8 flex flex-col gap-5 items-center">
         <SignUp signInUrl="/signin" />
       </div>
