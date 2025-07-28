4acefe4ef7fbe1a2801d42fa0f71254f92a59785
Bret Axl Sebastian Pura <cosmicdevcs@gmail.com>
2025-07-28 10:38:42 +0800

    remove buttons

--- src/app/signup/[[...rest]]/page.tsx
+++ src/app/signup/[[...rest]]/page.tsx
@@ -1,23 +1,11 @@
 "use client";
-import { SignedOut, SignUp } from "@clerk/nextjs";
-import Link from "next/link";
+import { SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-background px-4">
       <div className="p-8 flex flex-col gap-5 items-center">
         <SignUp signInUrl="/signin" />
-        <SignedOut>
-          <p className="text-sm text-muted-foreground mt-4">
-            Already have an account?{" "}
-            <Link
-              href="/signin"
-              className="text-primary underline hover:opacity-80"
-            >
-              Sign in
-            </Link>
-          </p>
-        </SignedOut>
       </div>
     </div>
   );
